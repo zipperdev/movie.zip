af8c5efe1b90fc0559414a4d5996013aaaf5f088
zipperdev <sh20101119@gmail.com>
2021-04-21 22:50:13 +0900

    a1.1
    Style video detail page and change the way to get API response request package to fetch API(Fetch API is more convenient and small.).

--- src/controllers.js
+++ src/controllers.js
@@ -1,44 +1,27 @@
-import request, { get } from "request";
-import routes from "./routes"
+import fetch from "node-fetch";
+import routes from "./routes";
 require("dotenv").config();
 
+const mainUri = "https://api.themoviedb.org/3";
+const API_KEY = "7633a8bbbc602917153dc162e10ee81a";
+
 export const home = (req, res) => {
-    request({
-        encoding: "utf-8", 
-        method: "GET", 
-        uri: "https://api.themoviedb.org/3/movie/popular?api_key=7633a8bbbc602917153dc162e10ee81a"
-    }, function(err, _, html) {
-        if (err) {
-            return res.render("error", { pageTitle: "500 Error" });
-        } else {
-            const pMovies = JSON.parse(html);
-            request({
-                encoding: "utf-8", 
-                method: "GET", 
-                uri: "https://api.themoviedb.org/3/tv/popular?api_key=7633a8bbbc602917153dc162e10ee81a"
-            }, function(err, _, html) {
-                if (err) {
-                    return res.render("error", { pageTitle: "500 Error" });
-                } else {
-                    const pTvShows = JSON.parse(html);
-                    request({
-                        encoding: "utf-8", 
-                        method: "GET", 
-                        uri: "https://api.themoviedb.org/3/trending/movies/day?api_key=7633a8bbbc602917153dc162e10ee81a"
-                    }, function(err, _, html) {
-                        if (err) {
-                            return res.render("error", { pageTitle: "500 Error" });
-                        } else {
-                            const tMovies = JSON.parse(html);
-                            request({
-                                encoding: "utf-8", 
-                                method: "GET", 
-                                uri: "https://api.themoviedb.org/3/trending/tv/day?api_key=7633a8bbbc602917153dc162e10ee81a"
-                            }, function(err, _, html) {
-                                if (err) {
-                                    return res.render("error", { pageTitle: "500 Error" });
-                                } else {
-                                    const tTvShows = JSON.parse(html);
+    fetch(`${mainUri}/movie/popular?api_key=${API_KEY}`)
+        .then((res) => res.json())
+        .then((data) => {
+            const pMovies = data;
+            fetch(`${mainUri}/tv/popular?api_key=${API_KEY}`)
+                .then((res) => res.json())
+                .then((data) => {
+                    const pTvShows = data;
+                    fetch(`${mainUri}/trending/movies/day?api_key=${API_KEY}`)
+                        .then((res) => res.json())
+                        .then((data) => {
+                            const tMovies = data;
+                            fetch(`${mainUri}/trending/tv/day?api_key=${API_KEY}`)
+                                .then((res) => res.json())
+                                .then((data) => {
+                                    const tTvShows = data;
                                     return res.render("home", {
                                         pageTitle: "Home", 
                                         pMovies: pMovies.results, 
@@ -46,159 +29,147 @@ export const home = (req, res) => {
                                         tMovies: tMovies.results, 
                                         tTvs: tTvShows.results, 
                                     });
-                                };
-                            });
-                        };
-                    });
-                };
-            });
-        };
-    });
+                                }).catch((err) => {
+                                    return res.render("error", { pageTitle: "500 Error" });
+                                });
+                        }).catch((err) => {
+                            return res.render("error", { pageTitle: "500 Error" });
+                        });
+                }).catch((err) => {
+                    return res.render("error", { pageTitle: "500 Error" });
+                });
+        }).catch((err) => {
+            return res.render("error", { pageTitle: "500 Error" });
+        });
 };
 
 export const movieDetail = (req, res) => {
     const { id } = req.params;
-    request({
-        encoding: "utf-8", 
-        method: "GET", 
-        uri: `https://api.themoviedb.org/3/movie/${id}?api_key=7633a8bbbc602917153dc162e10ee81a`
-    }, function(err, _, html) {
-        if (err) {
-            return res.render("error", { pageTitle: "500 Error" });
-        } else {
-            const movie = JSON.parse(html);
+    fetch(`${mainUri}/movie/${id}?api_key=${API_KEY}`)
+        .then((res) => res.json())
+        .then((data) => {
+            const movie = data;
             if (movie.success === false) {
                 return res.render("error", { pageTitle: "500 Error" });
             } else {
                 return res.render("videoDetail", { pageTitle: `${movie.title || movie.name}`, video: movie });
             };
-        };
-    });
+        }).catch((err) => {
+            return res.render("error", { pageTitle: "500 Error" });
+        });
 };
 
 export const tvShowDetail = (req, res) => {
     const { id } = req.params;
-    request({
-        encoding: "utf-8", 
-        method: "GET", 
-        uri: `https://api.themoviedb.org/3/tv/${id}?api_key=7633a8bbbc602917153dc162e10ee81a`
-    }, function(err, _, html) {
-        if (err) {
-            return res.render("error", { pageTitle: "500 Error" });
-        } else {
-            const tvShow = JSON.parse(html);
+    fetch(`${mainUri}/tv/${id}?api_key=${API_KEY}`)
+        .then((res) => res.json())
+        .then((data) => {
+            const tvShow = data;
             if (tvShow.success === false) {
                 return res.render("error", { pageTitle: "500 Error" });
             } else {
                 return res.render("videoDetail", { pageTitle: `${tvShow.title || tvShow.name}`, video: tvShow });
             };
-        };
-    });
+        }).catch((err) => {
+            return res.render("error", { pageTitle: "500 Error" });
+        });
 };
 
 export const seasonDetail = (req, res) => {
     const { tvShow, season } = req.params;
-    request({
-        encoding: "utf-8", 
-        method: "GET", 
-        uri: `https://api.themoviedb.org/3/tv/${tvShow}?api_key=7633a8bbbc602917153dc162e10ee81a`
-    }, function(err, _, html) {
-        if (err) {
-            return res.render("error", { pageTitle: "500 Error" });
-        } else {
-            const parsedTvShow = JSON.parse(html);
-            if (tvShow.success === false) {
+    fetch(`${mainUri}/tv/${tvShow}?api_key=${API_KEY}`)
+        .then((res) => res.json())
+        .then((data) => {
+            const parsedTvShow = data;
+            if (parsedTvShow.success === false) {
                 return res.render("error", { pageTitle: "500 Error" });
             } else {
-                request({
-                    encoding: "utf-8", 
-                    method: "GET", 
-                    uri: `https://api.themoviedb.org/3/tv/${tvShow}/season/${season}?api_key=7633a8bbbc602917153dc162e10ee81a`
-                }, function(err, _, html) {
-                    if (err) {
-                        return res.render("error", { pageTitle: "500 Error", });
-                    } else {
-                        const season = JSON.parse(html);
+                fetch(`${mainUri}/tv/${tvShow}/season/${season}?api_key=${API_KEY}`)
+                    .then((res) => res.json())
+                    .then((data) => {
+                        const season = data;
                         if (season.success === false) {
                             return res.render("error", { pageTitle: "500 Error" });
                         } else {
                             return res.render("seasonsDetail", { pageTitle: `${parsedTvShow.name || parsedTvShows.title || '"Unknown"'} : Season ${season}`, season, tvShowId: tvShow, tvShow: parsedTvShow });
                         };
-                    };
-                });
+                    }).catch((err) => {
+                        return res.render("error", { pageTitle: "500 Error" });
+                    });
             };
-        };
-    });
+        }).catch((err) => {
+            return res.render("error", { pageTitle: "500 Error" });
+        });
 };
 
 export const episodeDetail = (req, res) => {
     const { tvShow, season, episode } = req.params;
-    request({
-        encoding: "utf-8", 
-        method: "GET", 
-        uri: `https://api.themoviedb.org/3/tv/${tvShow}/season/${season}/episode/${episode}?api_key=7633a8bbbc602917153dc162e10ee81a`
-    }, function(err, _, html) {
-        if (err) {
-            return res.render("error", { pageTitle: "500 Error" });
-        } else {
-            const parsedEpisode = JSON.parse(html);
+    fetch(`${mainUri}/tv/${tvShow}/season/${season}/episode/${episode}?api_key=${API_KEY}`)
+        .then((res) => res.json())
+        .then((data) => {
+            const parsedEpisode = data;
             if (parsedEpisode.success === false) {
                 return res.render("error", { pageTitle: "500 Error" });
             } else {
                 return res.render("episodeDetail", { pageTitle: `Season ${season} Episode ${episode}`, episode: parsedEpisode });
             };
-        };
-    });
+        }).catch((err) => {
+            return res.render("error", { pageTitle: "500 Error" });
+        });
 };
 
 export const search = (req, res) => {
     const { keyword } = req.query;
-    let movies;
-    let tvShows;
     if (keyword.match(/\w/g)) {
-        request({
-            encoding: "utf-8", 
-            method: "GET", 
-            uri: `https://api.themoviedb.org/3/search/movie?api_key=7633a8bbbc602917153dc162e10ee81a&query=${keyword}&page=1`
-        }, function(err, _, html) {
-            if (err) {
-                return res.render("error", { pageTitle: "500 Error" });
-            } else {
-                movies = JSON.parse(html).results;
-                request({
-                    encoding: "utf-8", 
-                    method: "GET", 
-                    uri: `https://api.themoviedb.org/3/search/tv?api_key=7633a8bbbc602917153dc162e10ee81a&query=${keyword}&page=1`
-                }, function(err, _, html) {
-                    tvShows = JSON.parse(html).results;
-                    request({
-                        encoding: "utf-8", 
-                        method: "GET", 
-                        uri: `https://api.themoviedb.org/3/search/movie?api_key=7633a8bbbc602917153dc162e10ee81a&query=${keyword}&page=2`
-                    }, function(err, _, html) {
-                        if (err) {
+        fetch(`${mainUri}/search/movie?api_key=${API_KEY}&query=${keyword}&page=1`)
+            .then((res) => res.json())
+            .then((data) => {
+                let movies = data.results;
+                if (data.success === false) {
+                    return res.render("error", { pageTitle: "500 Error" });
+                } else {
+                    fetch(`${mainUri}/search/tv?api_key=${API_KEY}&query=${keyword}&page=1`)
+                        .then((res) => res.json())
+                        .then((data) => {
+                            let tvShows = data.results;
+                            if (data.success === false) {
+                                return res.render("error", { pageTitle: "500 Error" });
+                            } else {
+                                fetch(`${mainUri}/search/movie?api_key=${API_KEY}&query=${keyword}&page=2`)
+                                    .then((res) => res.json())
+                                    .then((data) => {
+                                        movies = movies.concat(data.results);
+                                        if (data.success === false) {
+                                            return res.render("error", { pageTitle: "500 Error" });
+                                        } else {
+                                            fetch(`${mainUri}/search/tv?api_key=${API_KEY}&query=${keyword}&page=2`)
+                                                .then((res) => res.json())
+                                                .then((data) => {
+                                                    tvShows = tvShows.concat(data.results);
+                                                    if (data.success === false) {
+                                                        return res.render("error", { pageTitle: "500 Error" });
+                                                    } else {
+                                                        return res.render("search", {
+                                                            pageTitle: "Search", 
+                                                            movies, 
+                                                            tvs: tvShows
+                                                        });
+                                                    };
+                                                }).catch((err) => {
+                                                    return res.render("error", { pageTitle: "500 Error" });
+                                                });
+                                        };
+                                    }).catch((err) => {
+                                        return res.render("error", { pageTitle: "500 Error" });
+                                    });
+                            };
+                        }).catch((err) => {
                             return res.render("error", { pageTitle: "500 Error" });
-                        } else {
-                            const twoMovies = JSON.parse(html).results;
-                            movies = movies.concat(twoMovies);
-                            request({
-                                encoding: "utf-8", 
-                                method: "GET", 
-                                uri: `https://api.themoviedb.org/3/search/tv?api_key=7633a8bbbc602917153dc162e10ee81a&query=${keyword}&page=2`
-                            }, function(err, _, html) {
-                                const twoTvShows = JSON.parse(html).results;
-                                tvShows = tvShows.concat(twoTvShows);
-                                return res.render("search", {
-                                    pageTitle: "Search", 
-                                    movies, 
-                                    tvs: tvShows
-                                });
-                            });
-                        };
-                    });
-                });
-            };
-        });
+                        });
+                };
+            }).catch((err) => {
+                return res.render("error", { pageTitle: "500 Error" });
+            });
     } else {
         return res.redirect(routes.home);
     }
