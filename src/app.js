a0ea18635117439a214c0f258276e816e5e1e3c4
zipperdev <sh20101119@gmail.com>
2021-04-24 17:52:35 +0900

    a3.7
    Change app.js code

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ app.use(
         }
     })
 );
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use("/contents", express.static(__dirname + "/contents"));
